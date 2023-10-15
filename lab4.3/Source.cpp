4735d5d1b2d56374510411f9c6b0cced9f6bd9f4
radelytskyipavlo <145326273+radelytskyipavlo@users.noreply.github.com>
2023-10-15 17:20:19 +0300

    Add files via upload

---
+++ lab4.3/Source.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+int main()
+{
+	double x, xp, xk, dx, a, b, c, F;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "a = "; cin >> a;
+	cout << "b = "; cin >> b;
+	cout << "c = "; cin >> c;
+	cout << fixed;
+	cout << "----------------------" << endl;
+	cout << "|" << setw(7) << "x" << " |"
+		<< setw(10) << "y" << " |" << endl;
+	cout << "----------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		if (a < 0 && x != 0)
+			F = a * x * x + b * b * x;
+		else
+			if (a > 0 && x == 0)
+				F = x - a / (x - c);
+			else
+				F = 1 + x / c;
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(3) << F
+			<< " |" << endl;
+		x += dx;
+	}
+	cout << "----------------------" << endl;
+	return 0;
+}
